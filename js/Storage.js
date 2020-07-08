6684b81efbf0891d2581b67e2372352f6aa5f198
dchiapa <diegochiapa@gmail.com>
2020-07-08 17:33:21 -0300

    class Storage y app general

---
+++ js/Storage.js
@@ -0,0 +1,10 @@
+class Storage {
+  add() {
+
+  }
+  delete() {
+  }
+  modify() {
+  }
+  read() { }
+}
---
+++ js/app.js
@@ -0,0 +1,16 @@
+// Variables
+const btnAgregar = document.querySelector('#btnTarea');
+const nuevaTarea = document.querySelector('#nuevaTarea');
+const btnBorrar = document.querySelector('.borrar');
+
+let tareas = [];
+
+// Listeners
+btnAgregar.addEventListener('click', agregarTarea);
+btnBorrar.addEventListener('click', borrarTarea);
+
+// Funciones
+function agregarTarea() {
+}
+function borrarTarea() {
+}
