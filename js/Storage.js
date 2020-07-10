7f88a1da50963da4784d39db43547ea9754c29c0
dchiapa <diegochiapa@gmail.com>
2020-07-10 18:16:58 -0300

    completar app, funcionamiento básico

--- js/Storage.js
+++ js/Storage.js
@@ -1,9 +1,24 @@
 class Storage {
-  add() {
+  add(elemento) {
+    this.elemento = elemento
+    this.tareas = this.read();
+    this.tareas.push({ 'tarea': `${this.elemento}`, 'estado': 0 });
+    localStorage.setItem('tareas', JSON.stringify(this.tareas));
   }
-  delete() {
+  delete(elemento) {
+    this.elemento = elemento
+    this.tareas = this.read();
+    this.tareas.splice(this.elemento, 1);
+    localStorage.setItem('tareas', JSON.stringify(this.tareas));
   }
-  modify() {
+  modify(elemento) {
+    this.tareas = this.read();
+    if (this.tareas[elemento].estado == 0) {
+      this.tareas[elemento].estado = 1;
+    } else {
+      this.tareas[elemento].estado = 0;
+    };
+    localStorage.setItem('tareas', JSON.stringify(this.tareas));
   }
   read() {
     return this.tareas = JSON.parse(localStorage.getItem('tareas'));
--- js/app.js
+++ js/app.js
@@ -4,9 +4,10 @@ const storage = new Storage();
 // Variables
 const btnAgregar = document.querySelector('#btnTarea');
 const nuevaTarea = document.querySelector('#nuevaTarea');
-const listaTareas = document.querySelector('#listaTareas');
+const listaTareas = document.querySelector('#listaTareas').firstElementChild;
 
 let tareas = [];
+let elemento = '';
 
 // Listeners
 window.addEventListener('load', cargarTareas);
@@ -19,19 +20,51 @@ listaTareas.addEventListener('click', e => {
   else if (e.target.classList.contains('tarea')) {
     completarTarea(e);
   }
-
 });
 
 // Funciones
-function agregarTarea() {
-}
-function borrarTarea() {
-  console.dir('Borrar Elemento');
-}
 function cargarTareas() {
+  while (listaTareas.firstChild) {
+    listaTareas.removeChild(listaTareas.firstChild);
+  }
+  let fragmento = document.createDocumentFragment();
+  let estado = '';
+  let orden = 0;
   tareas = storage.read();
-  console.dir(tareas);
+  tareas.forEach(tarea => {
+    if (tarea.estado == 0) {
+      estado = 'noVisible';
+    } else {
+      estado = '';
+    }
+    let li = document.createElement('li');
+    li.innerHTML = `
+    <img class="completa ${estado}" src="img/check.svg" alt="completado">
+    <span class="tarea" data-order=${orden} data-estado=${tarea.estado}>${tarea.tarea}</span>
+    <img class="borrar" src="img/delete.svg" alt="eliminar">
+    `;
+    fragmento.appendChild(li);
+    orden = orden + 1;
+  });
+  listaTareas.appendChild(fragmento);
+  if (window.innerWidth < 500) {
+    nuevaTarea.setAttribute('maxlength', '30');
+  } else if (window.innerWidth < 575) {
+    nuevaTarea.setAttribute('maxlength', '50');
+  }
+}
+function agregarTarea() {
+  storage.add(nuevaTarea.value);
+  cargarTareas();
+  nuevaTarea.value = '';
+}
+function borrarTarea(e) {
+  elemento = e.target.parentNode.firstChild.nextSibling.nextSibling.nextSibling.getAttribute('data-order');
+  storage.delete(elemento);
+  cargarTareas();
 }
 function completarTarea(e) {
-  e.target.parentNode.childNodes[1].classList.toggle('noVisible');
+  elemento = e.target.getAttribute('data-order');
+  storage.modify(elemento);
+  cargarTareas();
 }
